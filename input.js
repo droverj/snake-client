75391dab73393ea4bbe65692b74553888017f2a7
Jodie Drover <jodiedrover@live.com>
2022-09-09 14:49:36 -0400

    Snake is controlled by command strings sent to the server using the connection object

--- input.js
+++ input.js
@@ -1,5 +1,12 @@
+// input.js
+
+// stores the active TCP connection object
+let connection;
+
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,13 +16,25 @@ const setupInput = function() {
 };
 
 const handleUserInput = (key) => {
-  process.stdout.write(key);
   if (key === '\u0003') {
     process.exit();
   };
+  // connection("w" === "Move: up");
+  
+  if (key === "w") {
+    connection.write("Move: up");
+  }
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+  if (key === "d") {
+    connection.write("Move: right");
+  }
 };
 
 module.exports = {
   setupInput,
-  handleUserInput
 };
